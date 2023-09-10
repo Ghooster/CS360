62432e70373e93079dfb2e9f769606e3c64214c8
Kumar Arpit <kumararpit987@gmail.com>
2023-09-10 05:43:57 +0530

    Lighting remains @_@

--- Assignments/Assignment_2/Solution/Arpit_200532_Assignment2.js
+++ Assignments/Assignment_2/Solution/Arpit_200532_Assignment2.js
@@ -1,17 +1,21 @@
 var gl;
 var color;
 var animation;
+var canvas;
 var matrixStack = [];
+var vMatrix = mat4.create();
+var pMatrix = mat4.create();
 var mMatrix = mat4.create();
 var uMMatrixLocation;
+var uVMatrixLocation;
+var uPMatrixLocation;
 var aPositionLocation;
 var uColorLoc;
 var circleBuf;
 var circleIndexBuf;
 var sqVertexPositionBuffer;
 var sqVertexIndexBuffer;
-var mode = 1;
-var length;
+var height;
 
 var cubeBuf;
 var cubeIndexBuf;
@@ -24,13 +28,34 @@ var spVerts = [];
 var spIndicies = [];
 var spNormals = [];
 
+var eyePos = [0.0, 0.0, 2.5];
+var COI = [0.0, 0.0, 0.0];
+var viewUp = [0.0, 1.0, 0.0];
+
+var degreeL1 = 0.0;
+var degreeL0 = 0.0;
+var degreeM1 = 0.0;
+var degreeM0 = 0.0;
+var degreeR1 = 0.0;
+var degreeR0 = 0.0;
+var prevMouseLX = 0.0;
+var prevMouseLY = 0.0;
+var prevMouseMX = 0.0;
+var prevMouseMY = 0.0;
+var prevMouseRX = 0.0;
+var prevMouseRY = 0.0;
+
 const vertexShaderCode = `#version 300 es
-in vec2 aPosition;
+in vec3 aPosition;
 uniform mat4 uMMatrix;
+uniform mat4 uPMatrix;
+uniform mat4 uVMatrix;
 
 void main() 
 {
-    gl_Position = uMMatrix*vec4(aPosition,0.0,1.0);
+    mat4 projectionModelView;
+	projectionModelView=uPMatrix*uVMatrix*uMMatrix;
+    gl_Position = projectionModelView*vec4(aPosition,1.0);
     gl_PointSize = 2.0;
 }`;
 
@@ -107,7 +132,7 @@ function initShaders()
     return shaderProgram;
 }
 
-function initGL(canvas)
+function initGL()
 {
     try
     {
@@ -183,20 +208,10 @@ function drawSphere(color, mMatrix)
     gl.vertexAttribPointer(aPositionLocation, spBuf.itemSize, gl.FLOAT, false, 0, 0);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, spIndexBuf);
     gl.uniform4fv(uColorLoc, color);
-    switch (mode)
-    {
-        case 0:
-            gl.drawElements(gl.TRIANGLES, spIndexBuf.numItems, gl.UNSIGNED_INT, 0);
-            break;
-        case 1:
-            gl.drawElements(gl.LINE_LOOP, spIndexBuf.numItems, gl.UNSIGNED_INT, 0);
-            break;
-        case 2:
-            gl.drawElements(gl.POINTS, spIndexBuf.numItems, gl.UNSIGNED_INT, 0);
-            break;
-        default:
-            console.log("invalid mode");
-    }
+    gl.uniformMatrix4fv(uMMatrixLocation, false, mMatrix);
+    gl.uniformMatrix4fv(uVMatrixLocation, false, vMatrix);
+    gl.uniformMatrix4fv(uPMatrixLocation, false, pMatrix);
+    gl.drawElements(gl.TRIANGLES, spIndexBuf.numItems, gl.UNSIGNED_INT, 0);
 }
 
 function initCubeBuffer()
@@ -216,17 +231,11 @@ function initCubeBuffer()
     cubeBuf.numItems = vertices.length / 3;
   
     var normals = [
-        // Front face
         0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
-        // Back face
         0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0,
-        // Top face
         0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
-        // Bottom face
         0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0,
-        // Right face
         1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-        // Left face
         -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
     ];
     cubeNormalBuf = gl.createBuffer();
@@ -236,42 +245,12 @@ function initCubeBuffer()
     cubeNormalBuf.numItems = normals.length / 3;
 
     var indices = [
-        0,
-        1,
-        2,
-        0,
-        2,
-        3, // Front face
-        4,
-        5,
-        6,
-        4,
-        6,
-        7, // Back face
-        8,
-        9,
-        10,
-        8,
-        10,
-        11, // Top face
-        12,
-        13,
-        14,
-        12,
-        14,
-        15, // Bottom face
-        16,
-        17,
-        18,
-        16,
-        18,
-        19, // Right face
-        20,
-        21,
-        22,
-        20,
-        22,
-        23, // Left face
+        0, 1, 2, 0, 2, 3,
+        4, 5, 6, 4, 6, 7,
+        8, 9, 10, 8, 10, 11,
+        12, 13, 14, 12, 14, 15,
+        16, 17, 18, 16, 18, 19,
+        20, 21, 22, 20, 22, 23,
     ];
     cubeIndexBuf = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuf);
@@ -287,39 +266,112 @@ function drawCube(color, mMatrix)
     gl.vertexAttribPointer(aPositionLocation, cubeBuf.itemSize, gl.FLOAT, false, 0, 0);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuf);
     gl.uniform4fv(uColorLoc, color);
-    switch (mode)
+    gl.uniformMatrix4fv(uMMatrixLocation, false, mMatrix);
+    gl.uniformMatrix4fv(uVMatrixLocation, false, vMatrix);
+    gl.uniformMatrix4fv(uPMatrixLocation, false, pMatrix);
+    gl.drawElements(gl.TRIANGLES, cubeIndexBuf.numItems, gl.UNSIGNED_SHORT, 0);
+}
+
+function onMouseDown(event)
+{
+    document.addEventListener("mousemove", onMouseLMove, false);
+    document.addEventListener("mousemove", onMouseMMove, false);
+    document.addEventListener("mousemove", onMouseRMove, false);
+    document.addEventListener("mouseup", onMouseUp, false);
+    document.addEventListener("mouseout", onMouseOut, false);
+    if (event.layerX <= canvas.width && event.layerX >= 0 && event.layerY <= canvas.height && event.layerY >= 0)
     {
-        case 0:
-            gl.drawElements(gl.TRIANGLES, cubeIndexBuf.numItems, gl.UNSIGNED_SHORT, 0);
-            break;
-        case 1:
-            gl.drawElements(gl.LINE_LOOP, cubeIndexBuf.numItems, gl.UNSIGNED_SHORT, 0);
-            break;
-        case 2:
-            gl.drawElements(gl.POINTS, cubeIndexBuf.numItems, gl.UNSIGNED_SHORT, 0);
-            break;
-        default:
-            console.log("invalid mode");
+        prevMouseX = event.clientX;
+        prevMouseY = canvas.height - event.clientY;
     }
+  }
+  
+function onMouseLMove(event)
+{
+    if (event.layerX <= canvas.width/3 && event.layerX >= 0 && event.layerY <= canvas.height && event.layerY >= 0)
+    {
+        var mouseX = event.clientX;
+        var diffX1 = mouseX - prevMouseX;
+        prevMouseX = mouseX;
+        degreeL0 = degreeL0 + diffX1 / 2;
+        var mouseY = canvas.height - event.clientY;
+        var diffY2 = mouseY - prevMouseY;
+        prevMouseY = mouseY;
+        degreeL1 = degreeL1 - diffY2 / 2;
+        drawScene();
+    }
+}
+
+function onMouseMMove(event)
+{
+    if (event.layerX <= 2*canvas.width/3 && event.layerX >= canvas.width/3 && event.layerY <= canvas.height && event.layerY >= 0)
+    {
+        var mouseX = event.clientX;
+        var diffX1 = mouseX - prevMouseX;
+        prevMouseX = mouseX;
+        degreeM0 = degreeM0 + diffX1 / 2;
+        var mouseY = canvas.height - event.clientY;
+        var diffY2 = mouseY - prevMouseY;
+        prevMouseY = mouseY;
+        degreeM1 = degreeM1 - diffY2 / 2;
+        drawScene();
+    }
+}
+
+function onMouseRMove(event)
+{
+    if (event.layerX <= canvas.width && event.layerX >= 2*canvas.width/3 && event.layerY <= canvas.height && event.layerY >= 0)
+    {
+        var mouseX = event.clientX;
+        var diffX1 = mouseX - prevMouseX;
+        prevMouseX = mouseX;
+        degreeR0 = degreeR0 + diffX1 / 2;
+        var mouseY = canvas.height - event.clientY;
+        var diffY2 = mouseY - prevMouseY;
+        prevMouseY = mouseY;
+        degreeR1 = degreeR1 - diffY2 / 2;
+        drawScene();
+    }
+}
+
+function onMouseUp(event)
+{
+    document.removeEventListener("mousemove", onMouseLMove, false);
+    document.removeEventListener("mousemove", onMouseMMove, false);
+    document.removeEventListener("mousemove", onMouseRMove, false);
+    document.removeEventListener("mouseup", onMouseUp, false);
+    document.removeEventListener("mouseout", onMouseOut, false);
+}
+  
+function onMouseOut(event)
+{
+    document.removeEventListener("mousemove", onMouseLMove, false);
+    document.removeEventListener("mousemove", onMouseMMove, false);
+    document.removeEventListener("mousemove", onMouseRMove, false);
+    document.removeEventListener("mouseup", onMouseUp, false);
+    document.removeEventListener("mouseout", onMouseOut, false);
 }
 
 function drawSceneL()
 {
     gl.clearColor(211/255, 211/255, 238/255, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
     mat4.identity(mMatrix);
+    mMatrix = mat4.rotate(mMatrix, degToRad(degreeL0), [0, 1, 0]);
+    mMatrix = mat4.rotate(mMatrix, degToRad(degreeL1), [1, 0, 0]);
 
     pushMatrix(matrixStack, mMatrix);
     color = [0/255, 112/255, 163/255, 1];
-    mMatrix = mat4.translate(mMatrix, [0.0, 0.5, 0]);
-    mMatrix = mat4.scale(mMatrix, [0.25, 0.25, 0.25]);
+    mMatrix = mat4.translate(mMatrix, [0.0, 0.55, 0]);
+    mMatrix = mat4.scale(mMatrix, [0.3, 0.3, 0.3]);
     drawSphere(color, mMatrix);
-
     mMatrix = popMatrix(matrixStack);
+
     pushMatrix(matrixStack, mMatrix);
     color = [165/255, 166/255, 111/255, 1];
     mMatrix = mat4.translate(mMatrix, [0.0, -0.25, 0]);
-    mMatrix = mat4.scale(mMatrix, [0.5, 1, 0.5]);
+    mMatrix = mat4.scale(mMatrix, [0.6, 1, 0.6]);
     drawCube(color, mMatrix);
     mMatrix = popMatrix(matrixStack);
 }
@@ -328,7 +380,10 @@ function drawSceneM()
 {
     gl.clearColor(238/255, 211/255, 210/255, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
     mat4.identity(mMatrix);
+    mMatrix = mat4.rotate(mMatrix, degToRad(degreeM0), [0, 1, 0]);
+    mMatrix = mat4.rotate(mMatrix, degToRad(degreeM1), [1, 0, 0]);
 
     pushMatrix(matrixStack, mMatrix);
     color = [150/255, 150/255, 150/255, 1];
@@ -375,13 +430,16 @@ function drawSceneR()
 {
     gl.clearColor(211/255, 238/255, 212/255, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
     mat4.identity(mMatrix);
+    mMatrix = mat4.rotate(mMatrix, degToRad(degreeR0), [0, 1, 0]);
+    mMatrix = mat4.rotate(mMatrix, degToRad(degreeR1), [1, 0, 0]);
 
     // Upper cube
     pushMatrix(matrixStack, mMatrix);
     color = [148/255, 50/255, 18/255, 1];
     mMatrix = mat4.translate(mMatrix, [0.0, 0.45, 0]);
-    mMatrix = mat4.scale(mMatrix, [1.5, 0.05, 1]);
+    mMatrix = mat4.scale(mMatrix, [1.5, 0.05, 0.5]);
     drawCube(color, mMatrix);
     mMatrix = popMatrix(matrixStack);
 
@@ -405,7 +463,7 @@ function drawSceneR()
     pushMatrix(matrixStack, mMatrix);
     color = [148/255, 50/255, 18/255, 1];
     mMatrix = mat4.translate(mMatrix, [0.0, -0.45, 0]);
-    mMatrix = mat4.scale(mMatrix, [1.5, 0.05, 1]);
+    mMatrix = mat4.scale(mMatrix, [1.5, 0.05, 0.5]);
     drawCube(color, mMatrix);
     mMatrix = popMatrix(matrixStack);
 
@@ -462,35 +520,59 @@ function drawSceneR()
 function drawScene()
 {
     gl.enable(gl.SCISSOR_TEST);
-    gl.viewport(0*length, 0*length, 1*length, 1*length);
-    gl.scissor(0*length, 0*length, 1*length, 1*length);
+    mat4.identity(vMatrix);
+    vMatrix = mat4.lookAt(eyePos, COI, viewUp, vMatrix);
+    mat4.identity(pMatrix);
+    mat4.perspective(50, 1.0, 0.1, 1000, pMatrix);
+    
+    mat4.identity(mMatrix);
+    pushMatrix(matrixStack, mMatrix);
+    gl.viewport(0*height, 0, height, height);
+    gl.scissor(0*height, 0, height, height);
     drawSceneL();
-    gl.viewport(1*length, 0*length, 1*length, 1*length);
-    gl.scissor(1*length, 0*length, 1*length, 1*length);
+    mMatrix = popMatrix(matrixStack);
+    pushMatrix(matrixStack, mMatrix);
+    gl.viewport(1*height, 0, height, height);
+    gl.scissor(1*height, 0, height, height);
     drawSceneM();
-    gl.viewport(2*length, 0*length, 1*length, 1*length);
-    gl.scissor(2*length, 0*length, 1*length, 1*length);
+    mMatrix = popMatrix(matrixStack);
+    pushMatrix(matrixStack, mMatrix);
+    gl.viewport(2*height, 0, height, height);
+    gl.scissor(2*height, 0, height, height);
     drawSceneR();
+    mMatrix = popMatrix(matrixStack);
 }
 
 function webGLStart()
 {
-    var canvas = document.getElementById("Assignment2");
-    initGL(canvas);
+    canvas = document.getElementById("Assignment2");
+    document.addEventListener("mousedown", onMouseDown, false);
+    sliderL = document.getElementById("LightSliderId");
+    sliderL.addEventListener("input", LightSliderChanged);
+    sliderC = document.getElementById("CameraSliderId");
+    sliderC.addEventListener("input", CameraSliderChanged);
+    initGL();
     shaderProgram = initShaders();
     const aPositionLocation = gl.getAttribLocation(shaderProgram, "aPosition");
     uMMatrixLocation = gl.getUniformLocation(shaderProgram, "uMMatrix");
+    uVMatrixLocation = gl.getUniformLocation(shaderProgram, "uVMatrix");
+    uPMatrixLocation = gl.getUniformLocation(shaderProgram, "uPMatrix");
     gl.enableVertexAttribArray(aPositionLocation);
     uColorLoc = gl.getUniformLocation(shaderProgram, "color");
-    length = canvas.width/3;
+    height = canvas.width/3;
     initSphereBuffer();
     initCubeBuffer();
     gl.enable(gl.DEPTH_TEST);
     drawScene();
 }
 
-function mode_fun(m)
+function CameraSliderChanged()
 {
-    mode = m;
+    eyePos = [0.0, 0.0, parseFloat(sliderC.value)];
     drawScene();
+}
+
+function LightSliderChanged()
+{
+    console.log("Current Light slider value is", sliderL.value);
 }
